838e2659695bdc9f5fdda3d81c2d5aecad826a16
Blind <to_bee@icloud.com>
2021-11-09 22:12:36 +0300

    fixed ft_isalpha.c

--- Libft/Code/ft_isalpha.c
+++ Libft/Code/ft_isalpha.c
@@ -6,15 +6,24 @@
 /*   By: jdeclan <jdeclan@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 18:05:03 by jdeclan           #+#    #+#             */
-/*   Updated: 2021/11/09 19:05:25 by jdeclan          ###   ########.fr       */
+/*   Updated: 2021/11/09 22:09:55 by jdeclan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isalpha(int c)
+//#include<stdio.h>
+
+int	ft_isalpha(int c)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-    {
-        return 1;
-    }
-    return 0;
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
 }
+
+//int	main(void)
+//{
+//	printf("%d, %d, %d, %d", ft_isalpha(64),
+//		ft_isalpha(65), ft_isalpha(90), ft_isalpha(91));
+//	return (0);
+//}
