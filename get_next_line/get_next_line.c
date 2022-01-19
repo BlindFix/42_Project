c8957d7b1f66c8e37e2aab51a7218b176dce0590
Blind <to_bee@icloud.com>
2022-01-19 19:09:47 +0300

    hopefully final

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,13 +6,16 @@
 /*   By: jdeclan <jdeclan@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:57:04 by jdeclan           #+#    #+#             */
-/*   Updated: 2022/01/19 12:02:00 by jdeclan          ###   ########.fr       */
+/*   Updated: 2022/01/19 19:04:06 by jdeclan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
 
-static char	*ft_strjoin2(char *s1, char const *s2)
+static char	*ft_strjoin_free(char *s1, char const *s2)
 {
 	char	*newstr;
 	size_t	s1_len;
@@ -34,7 +37,7 @@ static char	*ft_strjoin2(char *s1, char const *s2)
 	return (newstr);
 }
 
-static int	ft_read(int fd, char *buf)
+static int	ft_read_w0(int fd, char *buf)
 {
 	int	len;
 
@@ -44,7 +47,7 @@ static int	ft_read(int fd, char *buf)
 	return (len);
 }
 
-static char	*ft_n_in_buf(char *buf, char *line)
+static char	*ft_eraseconcat(char *buf, char *line)
 {
 	char	*pocket_buf;
 	int		i;
@@ -63,27 +66,27 @@ static char	*ft_n_in_buf(char *buf, char *line)
 		ft_strlcpy(buf, buf + i + 1, BUFFER_SIZE + 1);
 	else
 		ft_memset(buf, 0, BUFFER_SIZE + 1);
-	line = ft_strjoin2(line, pocket_buf);
+	line = ft_strjoin_free(line, pocket_buf);
 	free(pocket_buf);
 	return (line);
 }
 
-static char	*ft_to_line(char *buf, int fd, int temp)
+static char	*ft_make_line(char *buf, int fd, int temp)
 {
 	char	*line;
 
 	line = (char *)ft_calloc(1, sizeof(char));
 	while (temp == BUFFER_SIZE && !ft_strchr(buf, '\n'))
 	{
-		line = ft_strjoin2(line, buf);
-		temp = ft_read(fd, buf);
+		line = ft_strjoin_free(line, buf);
+		temp = ft_read_w0(fd, buf);
 	}
 	if (temp < 0)
 	{
 		free(line);
 		return (NULL);
 	}
-	line = ft_n_in_buf(buf, line);
+	line = ft_eraseconcat(buf, line);
 	if (!line)
 		return (NULL);
 	if (line[0])
@@ -107,8 +110,8 @@ char	*get_next_line(int fd)
 			return (NULL);
 	}
 	if (!buf[0])
-		temp = ft_read(fd, buf);
-	line2 = ft_to_line(buf, fd, temp);
+		temp = ft_read_w0(fd, buf);
+	line2 = ft_make_line(buf, fd, temp);
 	if (!buf[0] || !line2)
 	{
 		free(buf);
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,27 +6,21 @@
 /*   By: jdeclan <jdeclan@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:54:30 by jdeclan           #+#    #+#             */
-/*   Updated: 2022/01/19 12:05:59 by jdeclan          ###   ########.fr       */
+/*   Updated: 2022/01/19 15:51:42 by jdeclan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h>
 
 char	*get_next_line(int fd);
-int		ft_read(int fd, char *buf);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *str, int c);
-void	*ft_memcpy(void *dest, const void *src, size_t count);
 void	*ft_memset(void *dest, int c, size_t len);
 void	*ft_calloc(size_t count, size_t size);
-char	*ft_strdup(const char *str1);
 size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
-char	*ft_strjoin2(char *s1, char const *s2);
 
 #endif
